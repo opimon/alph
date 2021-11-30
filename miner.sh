091d853b0c36a4f2c4b7cfef686a4197a7ad59a5
opimon <1458312+opimon@users.noreply.github.com>
2021-11-30 14:50:00 +0100

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,5 +1,5 @@
 echo "Launching the miner and restart automatically if it crashes"
-until ./bin/gpu-miner; do
+until ./bin/gpu-miner -a MINER_ADDRESS; do
     echo "Miner crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
