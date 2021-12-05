47b41b78f0165428d1114447cc7986e3e1c86932
opimon <1458312+opimon@users.noreply.github.com>
2021-12-05 11:09:22 +0100

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -5,8 +5,6 @@ apt install -y libuv1-dev git wget curl build-essential vim netcat
 
 git clone https://github.com/alephium/gpu-miner.git
 cd gpu-miner
-git checkout auto-switch-init
-git pull
 make
 
 wget https://raw.githubusercontent.com/opimon/alph/main/miner.sh
