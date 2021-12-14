741e4169a821ab9b40ce7c96059d58bb797f89a4
opimon <1458312+opimon@users.noreply.github.com>
2021-12-14 16:33:51 +0100

    Update pro_daniel_pool.sh

--- pro_daniel_pool.sh
+++ pro_daniel_pool.sh
@@ -19,7 +19,8 @@ apt update && apt install nodejs
 
 git clone https://github.com/alephium/mining-proxy.git
 cd mining-proxy
-sed -i 's/127\.0\.0\.1/18\.197\.149\.27/g' config.json 
+#sed -i 's/127\.0\.0\.1/18\.197\.149\.27/g' config.json
+sed -i 's/127\.0\.0\.1/52\.18\.87\.57/g' config.json
 sed -i 's/\[\]/\["1H7d2jHbJqwRugoga5SiALENZWabPbhqTvj7UNJSM9uUk","18tfVi6aSXPLt5XScV329krSgPsnvPe9V26xpgHrDNGjM","17Z6zCgKbpoW6T385stpnwKkQYoogrPvbhfUSDtuV1Mup","16k26kiei7557cz1FGVkR7bEPEXg8xxhjD4DJcqquBJgz"\]/g' config.json 
 npm install
 node init
