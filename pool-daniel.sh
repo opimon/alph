41429226493b3e275a0e146d9a74e40561c27e5c
opimon <1458312+opimon@users.noreply.github.com>
2021-12-17 22:53:06 +0100

    Create pool-daniel.sh

---
+++ pool-daniel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ ! -f config.json ]]
+then
+apt-get -y update && apt-get -y install wget vim
+wget https://github.com/alephium/gpu-miner/releases/download/v0.5.0/alephium-0.5.0-cuda-miner-linux
+wget https://github.com/alephium/mining-proxy/releases/download/v0.1.2/alephium-mining-proxy-0.1.2-linux
+wget https://github.com/alephium/gpu-miner/releases/download/v0.4.4/alephium-0.4.4-cuda-miner-linux
+
+cat <<EOT >> config.json
+{
+    "diff1TargetNumZero": 30,
+    "jobSize": 4096,
+    "serverHost": "eu.metapool.tech",
+    "serverPort": 20032,
+    "proxyPort": 30032,
+    "addresses": [
+    "1H7d2jHbJqwRugoga5SiALENZWabPbhqTvj7UNJSM9uUk",
+    "18tfVi6aSXPLt5XScV329krSgPsnvPe9V26xpgHrDNGjM",
+    "17Z6zCgKbpoW6T385stpnwKkQYoogrPvbhfUSDtuV1Mup",
+    "16k26kiei7557cz1FGVkR7bEPEXg8xxhjD4DJcqquBJgz"
+  ]
+}
+EOT
+chmod +x ale*
+fi
+while true; do ./alephium-mining-proxy-0.1.2-linux; done &
+while true; do ./alephium-0.5.0-cuda-miner-linux  -p 30032; ./alephium-0.4.4-cuda-miner-linux  -p 30032; sleep 1; done &
