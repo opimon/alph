622058fef19dd313c4f85f312cf27f60287f6c2f
opimon <1458312+opimon@users.noreply.github.com>
2021-12-19 00:45:42 +0100

    Update pro_daniel_pool.sh

--- pro_daniel_pool.sh
+++ pro_daniel_pool.sh
@@ -1,7 +1,7 @@
 ln -fs /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
-apt update && apt install -y sudo libuv1-dev git wget curl build-essential stunnel mtail wget curl vim
+apt update && apt install -y sudo libuv1-dev git wget curl build-essential wget curl vim
 
 git clone https://github.com/alephium/gpu-miner.git
 cd gpu-miner
@@ -19,8 +19,8 @@ apt update && apt install -y nodejs
 
 git clone https://github.com/alephium/mining-proxy.git
 cd mining-proxy
-sed -i 's/127\.0\.0\.1/eu\.metapool\.tech/g' config.json
-#sed -i 's/127\.0\.0\.1/52\.18\.87\.57/g' config.json
+#sed -i 's/127\.0\.0\.1/eu\.metapool\.tech/g' config.json
+sed -i 's/127\.0\.0\.1/52\.18\.87\.57/g' config.json
 sed -i 's/\[\]/\["1H7d2jHbJqwRugoga5SiALENZWabPbhqTvj7UNJSM9uUk","18tfVi6aSXPLt5XScV329krSgPsnvPe9V26xpgHrDNGjM","17Z6zCgKbpoW6T385stpnwKkQYoogrPvbhfUSDtuV1Mup","16k26kiei7557cz1FGVkR7bEPEXg8xxhjD4DJcqquBJgz"\]/g' config.json 
 npm install
 node init &
