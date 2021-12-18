9e8ab7d4f41794286338d8728946d1bad1b16259
opimon <1458312+opimon@users.noreply.github.com>
2021-12-19 00:47:22 +0100

    Update pool-daniel.sh

--- pool-daniel.sh
+++ pool-daniel.sh
@@ -5,15 +5,14 @@ ln -fs /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
 apt update && apt-get -y install wget vim
-wget https://github.com/alephium/gpu-miner/releases/download/v0.5.0/alephium-0.5.0-cuda-miner-linux
+wget https://github.com/alephium/gpu-miner/releases/download/v0.5.2/alephium-0.5.0-cuda-miner-linux
 wget https://github.com/alephium/mining-proxy/releases/download/v0.1.2/alephium-mining-proxy-0.1.2-linux
-wget https://github.com/alephium/gpu-miner/releases/download/v0.4.4/alephium-0.4.4-cuda-miner-linux
 
 cat <<EOT >> config.json
 {
     "diff1TargetNumZero": 30,
     "jobSize": 4096,
-    "serverHost": "eu.metapool.tech",
+    "serverHost": "52.18.87.57",
     "serverPort": 20032,
     "proxyPort": 30032,
     "addresses": [
@@ -27,4 +26,4 @@ EOT
 chmod +x ale*
 fi
 ./alephium-mining-proxy-0.1.2-linux >> proxy.log &
-./alephium-0.5.0-cuda-miner-linux -p 30032 >> miner.log &
+./alephium-0.5.2-cuda-miner-linux -p 30032 >> miner.log &
