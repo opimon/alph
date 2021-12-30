0ced56af71e643ebfc8f3410c4c63433106fe134
opimon <1458312+opimon@users.noreply.github.com>
2021-12-30 23:26:19 +0100

    Create metapool-mining.sh

---
+++ metapool-mining.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ ! -f config.json ]]
+then
+ln -fs /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
+
+apt update && apt-get -y install wget vim
+wget https://github.com/alephium/gpu-miner/releases/download/v0.5.4/alephium-0.5.4-cuda-miner-linux
+wget https://github.com/alephium/mining-proxy/releases/download/v0.2.0/alephium-mining-proxy-0.2.0-linux
+
+cat <<EOT >> config.json
+{
+    "serverHost": "eu.metapool.tech",
+    "serverPort": 20032,
+    "proxyPort": 30032,
+    "addresses": [
+    "your-mining-address-1",
+    "your-mining-address-2",
+    "your-mining-address-3",
+    "your-mining-address-4"
+  ]
+}
+EOT
+chmod +x ale*
+fi
+./alephium-mining-proxy-0.2.0-linux >> proxy.log &
+./alephium-0.5.4-cuda-miner-linux -p 30032 >> miner.log &
