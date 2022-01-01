57a5b7d7b1e6b569167d48f64ce6d4168af74ad3
opimon <1458312+opimon@users.noreply.github.com>
2022-01-01 18:19:44 +0100

    Update pool-daniel.sh

--- pool-daniel.sh
+++ pool-daniel.sh
@@ -5,14 +5,12 @@ ln -fs /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 
 apt update && apt-get -y install wget vim
-wget https://github.com/alephium/gpu-miner/releases/download/v0.5.3/alephium-0.5.3-cuda-miner-linux
+wget https://github.com/alephium/gpu-miner/releases/download/v0.5.4/alephium-0.5.4-cuda-miner-linux
 wget https://github.com/alephium/mining-proxy/releases/download/v0.1.2/alephium-mining-proxy-0.1.2-linux
 
 cat <<EOT >> config.json
 {
-    "diff1TargetNumZero": 30,
-    "jobSize": 4096,
-    "serverHost": "52.18.87.57",
+    "serverHost": "eu.metapool.tech",
     "serverPort": 20032,
     "proxyPort": 30032,
     "addresses": [
