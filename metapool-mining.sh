91e59c90e04b725ae304297c607371f15dbf02ee
opimon <daniel.grand@opilab.ch>
2022-01-07 20:14:36 +0100

    Update metapool-mining.sh

--- metapool-mining.sh
+++ metapool-mining.sh
@@ -8,14 +8,16 @@ wget https://github.com/alephium/mining-proxy/releases/download/v0.2.0/alephium-
 
 cat <<EOT >> config.json
 {
-    "serverHost": "eu.metapool.tech",
-    "serverPort": 20032,
-    "proxyPort": 30032,
-    "addresses": [
-    "your-mining-address-1",
-    "your-mining-address-2",
-    "your-mining-address-3",
-    "your-mining-address-4"
+    "diff1TargetNumZero": 30,                       
+    "jobSize": 4096,                                
+    "serverHost": "eu.metapool.tech",               
+    "serverPort": 20032,                            
+    "proxyPort": 30032,                             
+    "addresses": [                                  
+    "1H7d2jHbJqwRugoga5SiALENZWabPbhqTvj7UNJSM9uUk",
+    "18tfVi6aSXPLt5XScV329krSgPsnvPe9V26xpgHrDNGjM",
+    "17Z6zCgKbpoW6T385stpnwKkQYoogrPvbhfUSDtuV1Mup",
+    "16k26kiei7557cz1FGVkR7bEPEXg8xxhjD4DJcqquBJgz" 
   ]
 }
 EOT
