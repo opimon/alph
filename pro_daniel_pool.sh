0710a6a2427c457e0aa17995bff69eac1c7435b8
opimon <daniel.grand@opilab.ch>
2022-01-11 23:36:03 +0100

    Update pro_daniel_pool.sh

--- pro_daniel_pool.sh
+++ pro_daniel_pool.sh
@@ -19,8 +19,7 @@ apt update && apt install -y nodejs
 
 git clone https://github.com/alephium/mining-proxy.git
 cd mining-proxy
-sed -i 's/127\.0\.0\.1/eu\.metapool\.tech/g' config.json
-#sed -i 's/127\.0\.0\.1/52\.18\.87\.57/g' config.json
+sed -i 's/127\.0\.0\.1/pi\.metapool\.tech/g' config.json
 sed -i 's/\[\]/\["1H7d2jHbJqwRugoga5SiALENZWabPbhqTvj7UNJSM9uUk","18tfVi6aSXPLt5XScV329krSgPsnvPe9V26xpgHrDNGjM","17Z6zCgKbpoW6T385stpnwKkQYoogrPvbhfUSDtuV1Mup","16k26kiei7557cz1FGVkR7bEPEXg8xxhjD4DJcqquBJgz"\]/g' config.json 
 npm install
 node init &
